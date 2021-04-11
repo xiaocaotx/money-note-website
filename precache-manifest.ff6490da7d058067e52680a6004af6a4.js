8eb9018aa14a470ad7c54c48c1ec804249162d2d
遥远 <xiaocaotx@126.com>
2021-04-11 23:36:53 +0800

    deploy

---
+++ precache-manifest.ff6490da7d058067e52680a6004af6a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0bcec4c2d27837757fb",
+    "url": "/money-note-website/css/app.d6c977fb.css"
+  },
+  {
+    "revision": "157521316f6d54658b91246ab581ffc3",
+    "url": "/money-note-website/index.html"
+  },
+  {
+    "revision": "e0bcec4c2d27837757fb",
+    "url": "/money-note-website/js/app.5797e3d5.js"
+  },
+  {
+    "revision": "71a32ec5351e6ee893a0",
+    "url": "/money-note-website/js/chunk-vendors.0d022475.js"
+  },
+  {
+    "revision": "66667cbd4f4fc0d10cf880a46a1a4150",
+    "url": "/money-note-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-note-website/robots.txt"
+  }
+]);
